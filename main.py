d755aa2dd950e6a8498f1a692025c4e1dcfb2376
Gal Aviezri <71984608+gaviezri@users.noreply.github.com>
2023-11-28 11:39:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from datetime import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+driver = webdriver.Chrome()
+driver.get('https://bookings.zenchef.com/results?rid=358407')
+guestSelect = Select(driver.find_element(By.XPATH, "(//select)[1]"))
+dateInput = driver.find_element(By.XPATH, "(//input)[1]")
 
+guestSelect.select_by_index(1)
+dateInput.click()
+if(datetime.now().month == 11):
+    driver.find_element(By.XPATH, "(//div[@class='DayPicker-NavButton'])[1]").click()
+# figure out how to get the clickable element
+driver.find_element(By.CSS_SELECTOR,"div[aria-label='Monday, 18 December 2023']").click()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# move to december if needed
+# select 18, 19, 20
+# if "Service" is available
+# send email to me and gili
+driver.refresh()
+# dateInput.send_keys("Tue 19 Dec")
+# dateInput.send_keys("Wed 20 Dec")
+elem = driver.find_element(By.XPATH, "//div[contains(text(), 'service')]")
+input()
+def check_available_dates(restaurant_url):
+    # Send an HTTP request to the restaurant's website
+    response = requests.get(restaurant_url)
 
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Use BeautifulSoup to navigate and extract information about available dates
+        # This part depends on the specific structure of the website
+        # Example: find all elements with a certain class
+        available_dates = soup.find_all('span', class_='available-date')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Process and print the available dates
+        for date in available_dates:
+            print(date.text)
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
+# Example usage
+restaurant_url = 'https://bookings.zenchef.com/results?rid=358407'
+check_available_dates(restaurant_url)
